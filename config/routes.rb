8ca19f365a73246f7d2d0e8170fb46c1f7bbeebb
Tristan Villanueva <tan_vil@hotmail.com>
2015-10-30 16:45:53 -0400

    hid edit/destroy links, visible by employees only

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,11 @@ Rails.application.routes.draw do
   resources :players
   resources :employees
 
-  # get '/', to: 'employees#login'
   get '/', to: 'employees#login'
-  # get '/profile', to: 'employees#show'
-  post 'login_verify', to: 'employees#login_verify'
+  post '/login_verify', to: 'employees#login_verify'
   get '/show', to:'employees#show'
 
+  get '/login', to: 'employees#login'
   get '/logout', to: 'employees#logout'
   # get 'employees/', to: 'employees#login'
  
